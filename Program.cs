062118846a2e70e682046774531b0e43860e8d8b
Blair Young <blairyoung009@gmail.com>
2021-11-30 15:35:38 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace humans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+class Human
+{
+    // Fields for Human
+    public string Name;
+    public int Strength;
+    public int Intelligence;
+    public int Dexterity;
+    private int Health;
+    
+    
+    // add a public "getter" property to access health
+    public int GetHealth{
+        get {
+            return Health;
+            }
+    }
+     
+    // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+    public Human(string name)
+    {
+        Name = name;
+        Strength = 3;
+        Intelligence = 3;
+        Dexterity = 3;
+        Health = 100;
+    }
+    // Add a constructor to assign custom values to all fields
+    public Human(string name, int strength, int intelligence, int dexterity, int health)
+    {
+        Name = name;
+        Strength = strength;
+        Intelligence = intelligence;
+        Dexterity = dexterity;
+        Health = health;
+    } 
+    // Build Attack method
+    public int Attack(Human target)
+    {
+        return target.Health -= (Strength * 5);
+    }
+}
+
+
+}
